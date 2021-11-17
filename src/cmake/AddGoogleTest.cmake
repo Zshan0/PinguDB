a46c6136571325360fd4e756e853f3f9e0211cf0
Groverkss <groverkss@gmail.com>
2021-11-17 12:26:46 +0530

    Fix googletest download script

--- src/cmake/AddGoogleTest.cmake
+++ src/cmake/AddGoogleTest.cmake
@@ -12,7 +12,7 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
     include(DownloadProject)
     download_project(PROJ                googletest
 		     GIT_REPOSITORY      https://github.com/google/googletest.git
-		     GIT_TAG             release-1.8.0
+		     GIT_TAG             main
 		     UPDATE_DISCONNECTED 1
 		     QUIET
     )
@@ -24,7 +24,9 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
 else()
     include(FetchContent)
     FetchContent_Declare(googletest
-        GIT_REPOSITORY      https://github.com/google/googletest.git)
+        GIT_REPOSITORY      https://github.com/google/googletest.git
+        GIT_TAG             main
+        )
     FetchContent_GetProperties(googletest)
     if(NOT googletest_POPULATED)
         FetchContent_Populate(googletest)
